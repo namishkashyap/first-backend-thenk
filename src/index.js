c4156870e3078ea2e158422ca96081a2aefce140
Wollvo-Design <wollvodesign@gmail.com>
2024-03-17 16:25:10 +0530

    Adding Response and Error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // require("dotenv").config({ path: './env' })
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import express from "express";
 
+const app = express();
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at PORT:${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO DB connection error:", err)
+    })
 
 
 
